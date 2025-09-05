1ac6a7fd654491fb14ae56ddab21641c04f9d3de
tomoka1001 <tomo.101.2382@gmail.com>
2025-09-05 19:28:30 +0900

    inquirieテーブルマイグレーション、次モデル作成

---
+++ src/laravelapp/database/migrations/2025_09_05_183102_create_inquiries_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInquiriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inquiries', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 30);
+            $table->string('email', 255);
+            $table->integer('sex');
+            $table->integer('category');
+            $table->integer('area');
+            $table->text('message', 300);
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inquiries');
+    }
+}
